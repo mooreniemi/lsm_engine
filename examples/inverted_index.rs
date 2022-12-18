a546075f87d7bc31c62860eb221fa37c9e304904
alex <bigbadmath@gmail.com>
2022-12-17 22:33:59 -0500

    doc_store to lsm

--- examples/inverted_index.rs
+++ examples/inverted_index.rs
@@ -1,39 +1,62 @@
-use lsm_engine::LSMBuilder;
+use lsm_engine::{LSMBuilder, LSMEngine};
 
 use std::collections::{HashMap, HashSet};
 
+const DOC_MAX_ID: &str = "doc_id_max";
+
 type DocId = usize;
 type PostingList = Vec<DocId>;
-type DocContent = String;
 type Term = String;
 
-#[derive(Debug)]
 struct InvertedIndex {
     index: HashMap<Term, PostingList>,
-    doc_store: HashMap<DocId, DocContent>,
+    doc_store: LSMEngine,
+    doc_id_max: LSMEngine,
 }
 
 impl InvertedIndex {
     fn new() -> Self {
         Self {
             index: HashMap::new(),
-            doc_store: HashMap::new(),
+            doc_store: LSMBuilder::new().
+                segment_size(2000). // each sst file will have up to 2000 entries
+                inmemory_capacity(100). //store only 100 entries in memory
+                sparse_offset(20). //store one out of every 20 entries written into segments in memory
+                wal_path("/tmp/doc_store_wal.ndjson"). //path
+                build(),
+            doc_id_max: LSMBuilder::new().
+                segment_size(2000). // each sst file will have up to 2000 entries
+                inmemory_capacity(100). //store only 100 entries in memory
+                sparse_offset(20). //store one out of every 20 entries written into segments in memory
+                wal_path("/tmp/doc_id_max_wal.ndjson"). //path
+                build(),
         }
     }
 
     fn get_document(&mut self, doc_id: DocId) -> String {
-        self.doc_store.get(&doc_id).unwrap().to_string()
+        self.doc_store
+            .read(&doc_id.to_string().as_str())
+            .unwrap()
+            .unwrap()
     }
 
-    fn add_document(&mut self, document: &str) {
-        let doc_id = self.doc_store.values().len();
-        self.doc_store.insert(doc_id, document.to_string());
+    fn add_document(&mut self, document: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let doc_id = self
+            .doc_id_max
+            .read(DOC_MAX_ID)?
+            .unwrap_or("0".to_string())
+            .parse::<usize>()?;
+        self.doc_store
+            .write(doc_id.to_string(), document.to_string())?;
 
         let mut tokens = document.split_whitespace();
         while let Some(token) = tokens.next() {
             let entry = self.index.entry(token.to_string()).or_insert(vec![]);
             entry.push(doc_id);
         }
+        self.doc_id_max
+            .write(DOC_MAX_ID.to_string(), (doc_id + 1).to_string())?;
+        Ok(())
     }
 
     fn update_document(
@@ -44,7 +67,8 @@ impl InvertedIndex {
         // why don't we simply remove then add? because we want to preserve doc_id
         // we need a copy of the original content so we can find the plists
         let previous_content = self.get_document(doc_id).clone();
-        *self.doc_store.get_mut(&doc_id).unwrap() = new_content.to_string();
+        self.doc_store
+            .write(doc_id.to_string(), new_content.to_string())?;
         // we just remove it from the index
         let mut previous_tokens = previous_content.split_whitespace();
         let mut new_tokens = new_content.split_whitespace();
@@ -85,10 +109,7 @@ impl InvertedIndex {
 
     fn delete_document(&mut self, doc_id: DocId) -> Result<(), Box<dyn std::error::Error>> {
         let document = self.get_document(doc_id).clone();
-        // we don't remove it because the other ids would shift and disrupt O(1) gets
-        // self.doc_store.remove(doc_id);
-        // instead we just introduce empty content at the id
-        *self.doc_store.get_mut(&doc_id).unwrap() = "".to_string();
+        self.doc_store.delete(&doc_id.to_string().as_str())?;
         // then we remove it from the index having identified its posting lists
         let mut tokens = document.split_whitespace();
         while let Some(token) = tokens.next() {
@@ -119,10 +140,9 @@ impl InvertedIndex {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut ii = InvertedIndex::new();
-    ii.add_document("some document i added");
-    ii.add_document("some other document i added");
-    ii.add_document("no tokens in common");
-    dbg!(&ii);
+    ii.add_document("some document i added")?;
+    ii.add_document("some other document i added")?;
+    ii.add_document("no tokens in common")?;
     let results = ii.search("added");
     dbg!(&results);
     ii.delete_document(1)?;
@@ -131,26 +151,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ii.update_document(2, "tokens in common added")?;
     let results = ii.search("added");
     dbg!(&results);
-
-    let mut lsm = LSMBuilder::new().
-        segment_size(2000). // each sst file will have up to 2000 entries
-        inmemory_capacity(100). //store only 100 entries in memory
-        sparse_offset(20). //store one out of every 20 entries written into segments in memory
-        wal_path("/tmp/inverted_index_rs_wal.ndjson"). //path
-        build();
-
-    let dataset = vec![
-        ("k1", vec![1, 2, 3]),
-        ("k2", vec![4, 5, 6]),
-        ("k1", vec![7, 8, 9]),
-    ];
-
-    for (k, v) in dataset.iter() {
-        lsm.write(String::from(*k), serde_json::to_string(v)?)?;
-    }
-
-    let k1: Vec<i32> = serde_json::from_str(lsm.read("k1")?.unwrap().as_str())?;
-    dbg!(&k1);
 
     Ok(())
 }
